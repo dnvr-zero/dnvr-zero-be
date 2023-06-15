6ead53b0eb66998970fcc085555a82d0338c5671
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-06-15 17:33:14 -0600

    connects to mongoDb database

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
-import express, { json } from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
 import items from './items';
+require('dotenv/config');
 
 const app = express();
 
-app.use(json())
+// app.use(json())
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', async (request, response) => {
-    response.json({ status: true, message: "Our node.js app works" })
+    response.send("Our node.js app works")
 });
 
 app.get('/items', (request, response) => {
     response.json({ status: true, message: "Fetched all items", data: items })
 })
+
+// Database connection
+mongoose
+    .connect(process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected "))
+    .catch((err) => console.log('error'));
 
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
