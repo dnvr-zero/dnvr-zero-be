4a1542918080811c49d0b12e8df2b34bb06783a3
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-06-17 19:55:08 -0600

    begins to document endpoints using swagger

--- src/app.js
+++ src/app.js
@@ -1,23 +1,68 @@
 import express, { json } from 'express';
 import mongoose from 'mongoose';
-// import items from './items';
-require('dotenv/config');
+import swaggerJsdoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+require("dotenv/config")
 
-const app = express();
+const app = express()
 
-const taskRouter = require("./routes/tasks");
+const taskRouter = require("./routes/tasks")
 
-app.use(express.json());
-app.use("/task", taskRouter);
+app.use(express.json())
+app.use("/task", taskRouter)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.get('/', async (request, response) => {
-    response.send("Our node.js app works")
-});
+const options = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        components: {
+            schemas: {
+                TaskItem: {
+                    type: "object",
+                    required: ["name"],
+                    properties: {
+                        name: {
+                            type: "string",
+                            example: "username",
+                        },
+                        description: {
+                            type: "string",
+                            example: "a really cool description of the task",
+                        },
+                        points: {
+                            type: "string",
+                            example: "50 points",
+                        },
+                        createdby: {
+                            type: "string",
+                            example: "Bob Robbedsky"
+                        }
+                    },
+                },
+            },
+        },
+        info: {
+            title: "dnvr-zero-be",
+            summary: "the backend endpoints for the SOA of dnvr-zero",
+            description:
+                "documentation for the available endpoints to retrieve, udpate, delete data from dnvr-zero-be",
+            version: "3.0.0",
+            contact: {
+                name: "Michael Marchand",
+            },
+            servers: ["http://localhost:3000"],
+        },
+    },
+    apis: ["src/routes/*.js"],
+}
 
-app.get('/items', (request, response) => {
-    response.json({ status: true, message: "Fetched all items", data: items })
+const swaggerDocs = swaggerJsdoc(options)
+app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+// Routes
+app.get("/", async (request, response) => {
+    response.send("The node.js app works")
 })
 
 // Database connection
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,43 +1,75 @@
-import express from 'express';
-const router = express.Router();
-const Tasks = require("../models/Tasks");
+import express from "express"
 
+const router = express.Router()
+const Tasks = require("../models/Tasks")
+
+// Routes
+/**
+ * @swagger
+ * /task:
+ *  get:
+ *    tags:
+ *      - [task]
+ *    summary: So this is where it goes....
+ *    description: Use to request all tasks
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ * */
 router.get("/", (request, response) => {
-  Tasks.find()
-    .then((resp) => response.status(200).json(resp))
-    .catch((err) => response.status(400).json("Request Failed"));
-});
+    Tasks.find()
+        .then((resp) => response.status(200).json(resp))
+        .catch((err) => response.status(400).json("Request Failed"))
+})
 
+/**
+ * @swagger
+ * /task:
+ *  post:
+ *    tags:
+ *      - [task]
+ *    summary: never you mind
+ *    description: Use to create a new task
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/TaskItem'
+ *    responses:
+ *      '201':
+ *        description: Task successfully created
+ *      '400':
+ *        description: Missing required field
+ * */
 router.post("/", (request, response) => {
-  const { name, createdby, description, points } = request.body;
-  const task = new Tasks({
-    name,
-    createdby,
-    description,
-    points,
-  });
-  task
-    .save()
-    .then((resp) => response.status(201).json(resp))
-    .catch((err) => response.status(400).json("Request Failed"));
-});
+    const { name, createdby, description, points } = request.body
+    const task = new Tasks({
+        name,
+        createdby,
+        description,
+        points,
+    })
+    task.save()
+        .then((resp) => response.status(201).json(resp))
+        .catch((err) => response.status(400).json("Request Failed"))
+})
 
 router.delete("/:id", (request, response) => {
-  Tasks.deleteOne({ _id: request.params.id })
-    .then((resp) => response.status(200).json(resp))
-    .catch((err) => response.status(400).json("Request Failed"));
-});
+    Tasks.deleteOne({ _id: request.params.id })
+        .then((resp) => response.status(200).json(resp))
+        .catch((err) => response.status(400).json("Request Failed"))
+})
 
 router.get("/:id", (request, response) => {
-  Tasks.findById(request.params.id)
-    .then((resp) => response.status(200).json(resp))
-    .catch((err) => response.status(400).json("Request Failed"));
-});
+    Tasks.findById(request.params.id)
+        .then((resp) => response.status(200).json(resp))
+        .catch((err) => response.status(400).json("Request Failed"))
+})
 
 router.patch("/:id", (request, response) => {
-  Tasks.updateOne({ _id: request.params.id }, { $set: request.body })
-    .then((resp) => response.status(200).json(resp))
-    .catch((err) => response.status(400).json("Request Failed"));
-});
+    Tasks.updateOne({ _id: request.params.id }, { $set: request.body })
+        .then((resp) => response.status(200).json(resp))
+        .catch((err) => response.status(400).json("Request Failed"))
+})
 
-module.exports = router;
+module.exports = router
