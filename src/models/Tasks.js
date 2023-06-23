37b3faba8ff5adccb55db04394d59fe63650b3e6
Elihu Rodriguez <elihu.rodriguez@outlook.com>
2023-06-23 14:57:55 -0700

    Renamed creatby and group_id to camel case variants.

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 // const mongoose = require("mongoose")
 
 const TaskSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    createdby: {
-        type: String,
-        require: false,
-    },
-    description: {
-        type: String,
-        required: false,
-    },
-    points: {
-        type: String,
-        required: false,
-    },
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: String,
+    require: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  points: {
+    type: String,
+    required: false,
+  },
+});
 
-module.exports = mongoose.model("Tasks", TaskSchema)
+module.exports = mongoose.model("Tasks", TaskSchema);
